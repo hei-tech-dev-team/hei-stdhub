db56fca73304f0ec4103c8259f6e9bd63cbdd59a
fatratra-png <nyfatratrar@gmail.com>
2026-02-23 10:38:12 +0300

    feat:ChatPage

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,13 @@
+import Sidebar      from "../components/layout/Sidebar";
+import ChatLayout   from "../components/chat/ChatLayout";
+
+export default function ChatPage() {
+  return (
+    <div className="flex min-h-screen bg-navy-dark">
+      <Sidebar />
+      <div className="flex-1 overflow-hidden">
+        <ChatLayout />
+      </div>
+    </div>
+  );
+}
