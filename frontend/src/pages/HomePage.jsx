edd7702a8531971165756b33c27e65f9778f5018
fatratra-png <nyfatratrar@gmail.com>
2026-02-23 10:37:08 +0300

    feat:HomePage

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import Sidebar      from "../components/layout/Sidebar";
+import TeacherHome  from "../components/dashboard/TeacherHome";
+import StudentHome  from "../components/dashboard/StudentHome";
+import { useAuth }  from "../context/AuthContext";
+
+export default function HomePage() {
+  const { user } = useAuth();
+
+  return (
+    <div className="flex min-h-screen bg-surface">
+      <Sidebar />
+      <main className="flex-1 flex flex-col min-h-screen overflow-hidden">
+        {user?.role === "teacher"
+          ? <TeacherHome />
+          : <StudentHome />
+        }
+      </main>
+    </div>
+  );
+}
