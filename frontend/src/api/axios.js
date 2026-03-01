914e8d17f27a5f761f90b476b1fe5c19930d3be2
Fatratra <nyfatratrar@gmail.com>
2026-03-01 19:43:06 +0300

    tryna fix login issue

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
     ? import.meta.env.VITE_API_URL + "/api"
-    : "http://localhost:3001/api",
+    : "https://hei-stdhub-backend.onrender.com/api/auth/login",
 });
 
 // Injecte le token JWT automatiquement
