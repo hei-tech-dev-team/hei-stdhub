ad4a6463268a4531be9766cdcc3354c6f7d08777
Fatratra <nyfatratrar@gmail.com>
2026-03-31 15:32:58 +0300

    chore:integrate the suggestions feat for bde in the backend+add suggestions routes into server.js

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
+app.use("/api/suggestions", require("./routes/suggestions"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/posts", require("./routes/posts"));
 app.use("/api/supports", require("./routes/supports"));
