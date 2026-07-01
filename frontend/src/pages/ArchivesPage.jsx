a9c347bea57d5c8274dbf77a08700683961124cc
fatratra-png <nyfatratrar@gmail.com>
2026-07-01 12:50:59 +0300

    redesign archives page with improved aesthetics and mobile responsiveness

--- frontend/src/pages/ArchivesPage.jsx
+++ frontend/src/pages/ArchivesPage.jsx
@@ -44,7 +44,6 @@ export default function ArchivesPage() {
                 <ArchiveGrid />
               </div>
             </div>
-            <ArchiveGrid />
           </div>
         </div>
       </main>
